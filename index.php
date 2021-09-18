203ad31f07951ff476ae9776d30d11bac15817f7
petrovich2308 <osgard.avs.dev@gmail.com>
2021-09-18 09:55:46 +0300

    - autoloading classes

--- index.php
+++ index.php
@@ -8,3 +8,11 @@ session_start();
 
 require_once 'config.php';
 require_once 'core/base/settings/internal_settings.php';
+
+function load($classname)
+{
+    $classname = str_replace('\\', '/', $classname);
+    include $classname . '.php';
+}
+
+spl_autoload_register('load');
